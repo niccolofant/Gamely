cd13bb2491d119977b3e4c349b9be2b5f11de2dd
ocintnaf <ocintnaf@gmail.com>
2021-09-10 12:27:27 +0200

    Updated FeesStorage tests

--- test/testStorage.test.js
+++ test/testStorage.test.js
@@ -1,26 +1,35 @@
 const FeesStorage = artifacts.require("FeesStorage");
 const Ownable = artifacts.require("Ownable");
 
-contract("FeesStorage", (accounts) => {
+contract("FeesStorage", () => {
   let storageInstance;
   let ownableInstance;
+  let accounts;
+  let owner;
 
-  beforeEach(async () => {
+  before(async () => {
     storageInstance = await FeesStorage.deployed();
     ownableInstance = await Ownable.deployed();
-    //console.log(await ownableInstance.owner());
-
-    await storageInstance.sendTransaction({
-      from: accounts[1],
-      value: "100",
-    });
+    accounts = await web3.eth.getAccounts();
+    owner = await storageInstance.owner();
   });
 
   describe("FeesStorage", async () => {
-    it("Allows the Owner to retrieve current balance", async () => {
+    it("Initial balance should be 0 ETHs", async () => {
       let balance = await storageInstance.getBalance();
-      console.log(balance.toNumber());
-      assert.strictEqual(10, balance);
+      assert.strictEqual(0, balance.toNumber());
     });
+
+    it("Should display 1 ETH as balance after sending 1 ETH", async () => {
+      await web3.eth.sendTransaction({
+        to: storageInstance.address,
+        from: accounts[0],
+        value: web3.utils.toWei("1", "ether"),
+      });
+      let balance = await web3.eth.getBalance(storageInstance.address);
+      assert.strictEqual(web3.utils.toWei("1", "ether"), balance);
+    });
+
+    it("Should allow the owner to withdraw some ETHs", async () => {});
   });
 });
