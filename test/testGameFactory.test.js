14161847a9afabe25e5055b20e5a0f299d2ba74d
ocintnaf <ocintnaf@gmail.com>
2021-09-15 08:08:34 +0200

    Update testGameFactory.test.js

--- test/testGameFactory.test.js
+++ test/testGameFactory.test.js
@@ -5,20 +5,22 @@ contract("GameFactory", (accounts) => {
   let gameInstance;
 
   before(async () => {
-    factoryInstance = await GameFactory.deployed();
+    factoryInstance = await GameFactory.new();
   });
 
-  it("Should assert true", async () => {
-    await GameFactory.deployed();
-    return assert.isTrue(true);
-  });
-
-  it("Can deploy games", async () => {
-    await factoryInstance.instanciateGame({
-      from: accounts[0],
+  describe("GameFactory", async () => {
+    it("Should assert true", async () => {
+      await GameFactory.deployed();
+      return assert.isTrue(true);
     });
-    let gameAddress = await factoryInstance.getDeployedGames();
-    gameInstance = gameAddress[0];
-    assert.ok(gameInstance);
+
+    it("Can deploy games", async () => {
+      await factoryInstance.instanciateGame({
+        from: accounts[0],
+      });
+      let gameAddress = await factoryInstance.getDeployedGames();
+      gameInstance = gameAddress[0];
+      assert.ok(gameInstance);
+    });
   });
 });
