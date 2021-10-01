a4aa77f32a3d7d4df61da6588fc43bbb7c7d053d
ocintnaf <ocintnaf@gmail.com>
2021-10-01 15:07:35 +0200

    Update MainPage.js

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,28 +1,55 @@
 import image from "../images/main-page-image.svg";
 import AppButton from "./AppButton";
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 function MainPage() {
   return (
-    <div
-      style={{
-        padding: "5vw 10vw 0 10vw",
-      }}
-    >
+    <Box>
       <Grid container>
-        <Grid item xs={6}>
-          <Typography variant="h2" gutterBottom>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            textAlign: "left",
+          }}
+        >
+          <Typography
+            variant="h2"
+            gutterBottom
+            sx={{
+              fontFamily: "Roboto Mono",
+              fontWeight: "300",
+            }}
+          >
             Stake and earn ETHs while playing{" "}
             <span style={{ color: "#5479F7" }}>Mobile Games</span>.
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: "Roboto Mono",
+              color: "#777",
+              fontWeight: "200",
+              marginBottom: "2vw",
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </Typography>
 
           <AppButton />
         </Grid>
-        <Grid item xs={6}>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            textAlign: "right",
+          }}
+        >
           <img src={image} alt="Main Logo" />
         </Grid>
       </Grid>
-    </div>
+    </Box>
   );
 }
 
