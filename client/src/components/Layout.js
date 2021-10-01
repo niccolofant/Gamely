bf33b81e1f2e5f5334e9af44008ed2a37630c2e2
ocintnaf <ocintnaf@gmail.com>
2021-10-01 14:28:43 +0200

    Update Layout.js

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -7,19 +7,15 @@ import ConnectButton from "./ConnectButton";
 function Layout(props) {
   return (
     <React.Fragment>
-      <Box>
-        <Grid
-          container
-          spacing={5}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            padding: "20px",
-          }}
-        >
+      <Box
+        sx={{
+          padding: "2vw 0 0 10vw",
+        }}
+      >
+        <Grid container>
           <Grid
             item
-            xs={8}
+            xs={5}
             sx={{
               textAlign: "left",
             }}
@@ -28,7 +24,7 @@ function Layout(props) {
           </Grid>
           <Grid
             item
-            xs={4}
+            xs={5}
             sx={{
               textAlign: "right",
             }}
