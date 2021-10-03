f0aba3b186ef49a74ee03a8ef40127a7686dfb34
ocintnaf <ocintnaf@gmail.com>
2021-10-03 15:44:33 +0200

    Update GamelyApp.js

--- client/src/components/GamelyApp.js
+++ client/src/components/GamelyApp.js
@@ -5,24 +5,18 @@ import GamelySmallCard from "./GamelySmallCard";
 import GameCard from "./GameCard";
 import { storage } from "../abi/FeesStorageABI";
 import { gameFactory } from "../abi/GameFactoryABI";
+import { game } from "../abi/GameABI";
 import Web3 from "web3";
 import authContext from "./authContext";
 
 const web3 = new Web3(Web3.givenProvider);
 
-//GameFactory contract config
 const factoryAddress = "0x0576957cFb1C214E2Bb2B16560EdE2be512A6dc9";
 const factoryContract = new web3.eth.Contract(gameFactory, factoryAddress);
-
-//FeesStorage contract config
-const storageAddress = "0xeAB4dD34Ddba1dC79BE807605C11294aBb5443c7";
-const storageContract = new web3.eth.Contract(storage, storageAddress);
 
 function GamelyApp() {
   const [games, setGames] = useState("");
 
-  //const auth = useContext(authContext);
-  //console.log(typeof Object.keys(games));
   useEffect(async () => {
     let game = await factoryContract.methods.getDeployedGames().call();
     setGames(game);
@@ -30,19 +24,24 @@ function GamelyApp() {
 
   return (
     <Box sx={{ padding: "5vw" }}>
-      <GamelyMainCard>
-        <GamelySmallCard title="Games Created" number={games.length} />
-        <GamelySmallCard title="Total ETHs won" number="10" />
-      </GamelyMainCard>
-      {Object.values(games).map((game) => {
-        return <GameCard key={game} item={game} />;
-      })}
+      <Box sx={{ margin: "0 0 10vw 0" }}>
+        <GamelyMainCard>
+          <GamelySmallCard title="Games Created" number={games.length} />
+          <GamelySmallCard title="Total ETHs won" number="10" />
+        </GamelyMainCard>
+      </Box>
+      <Box>
+        <Typography
+          sx={{ fontFamily: "Roboto Mono", color: "#777", fontWeight: "300" }}
+        >
+          Available games ({games.length})
+        </Typography>
+        {Object.values(games).map((game) => {
+          return <GameCard key={game} item={game} />;
+        })}
+      </Box>
     </Box>
   );
 }
 
 export default GamelyApp;
-
-/*
- 
-      */
