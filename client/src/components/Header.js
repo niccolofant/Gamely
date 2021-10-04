fa5ac13692971e07b05d2954196822e920a0898f
ocintnaf <ocintnaf@gmail.com>
2021-10-04 20:06:06 +0200

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,27 @@
+import { Typography } from "@mui/material";
 import logo from "../images/logo.svg";
 
 function Header() {
   return (
-    <header>
-      <img src={logo} alt="Gamely Logo" height="50" />
+    <header
+      style={{
+        display: "flex",
+        alignItems: "center",
+        padding: "10px 10px 10px 0",
+      }}
+    >
+      <img src={logo} alt="Gamely Logo" height="30" />
+      <Typography
+        variant="h5"
+        sx={{
+          fontFamily: "Poppins",
+          fontWeight: "500",
+          color: "#222823",
+          padding: "0 0 0 0.5vw",
+        }}
+      >
+        Gamely
+      </Typography>
     </header>
   );
 }
