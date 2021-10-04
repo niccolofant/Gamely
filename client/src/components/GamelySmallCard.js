20c3d18d63b741930a5c39afc3e5e56c5353f866
ocintnaf <ocintnaf@gmail.com>
2021-10-04 20:05:41 +0200

    Add API call to get ETHs price

--- client/src/components/GamelySmallCard.js
+++ client/src/components/GamelySmallCard.js
@@ -1,7 +1,33 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
+const request = require("request");
+
+async function getEthPrice() {
+  const data = await request.get(
+    "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
+  );
+  return data.json();
+}
+(async () => {
+  const result = await getEthPrice();
+  console.log(result);
+})();
+
+/*
+function getEthPrice() {
+  request.get(
+    "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD",
+    function (error, response, body) {
+      if (error) return error;
+      else if (!response) return new Error("no response");
+      else if (response.statusCode !== 200) return new Error("bad response");
+      else return JSON.stringify(JSON.parse(body), null, 4);
+    }
+  );
+}
+*/
 function GamelySmallCard(props) {
-  const { title, number } = props;
+  const { title, number, currency } = props;
 
   return (
     <Box
@@ -24,6 +50,7 @@ function GamelySmallCard(props) {
         {title}
       </Typography>
       <Typography variant="h2" sx={{ fontFamily: "Roboto Mono" }}>
+        {currency}
         {number}
       </Typography>
     </Box>
