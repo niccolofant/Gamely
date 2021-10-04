6fcc217075ac2240f9d768f71e2c85c9f00e5c12
ocintnaf <ocintnaf@gmail.com>
2021-10-04 20:06:15 +0200

    Create MainPageCard.js

---
+++ client/src/components/MainPageCard.js
@@ -0,0 +1,75 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { FaCheck } from "react-icons/fa";
+import WinCard from "./WinCard";
+
+function MainPageCard() {
+  return (
+    <Box
+      sx={{
+        background: "#f5f5f5",
+        borderRadius: "15px",
+        boxShadow:
+          "0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 0 rgba(0, 127, 255, 0)",
+      }}
+    >
+      <Grid container>
+        <Grid item xs="7" sx={{ padding: "7vw 5vw 0 5vw" }}>
+          <Typography
+            variant="h3"
+            gutterBottom
+            sx={{
+              fontFamily: "Roboto Mono",
+              fontWeight: "300",
+              color: "#222823",
+            }}
+          >
+            Earning ETHs has never been easier
+          </Typography>
+          <Typography
+            variant="h6"
+            gutterBottom
+            sx={{
+              fontFamily: "Roboto Mono",
+              fontWeight: "200",
+              color: "#777",
+            }}
+          >
+            Play and win!
+          </Typography>
+          <Typography
+            variant="h7"
+            gutterBottom
+            sx={{
+              fontFamily: "Roboto Mono",
+              fontWeight: "300",
+              color: "#777",
+            }}
+          >
+            <Box sx={{ margin: "1vw 0 1vw 0" }}>
+              <FaCheck
+                style={{
+                  color: "#5479F7",
+                }}
+              />{" "}
+              Lowest fees — only 2%
+            </Box>
+            <Box>
+              <FaCheck
+                style={{
+                  color: "#5479F7",
+                }}
+              />{" "}
+              Secure
+            </Box>
+          </Typography>
+        </Grid>
+
+        <Grid item xs="5" sx={{ padding: "5vw" }}>
+          <WinCard />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default MainPageCard;
