9284e0fd24d4044af22372153af0abfa8a1fc806
ocintnaf <ocintnaf@gmail.com>
2021-10-05 13:06:45 +0200

    Update MainPage.js

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,6 +2,7 @@ import image from "../images/main-page-image.svg";
 import AppButton from "./AppButton";
 import MainPageCard from "./MainPageCard";
 import { Box, Grid, Typography } from "@mui/material";
+import crlogo from "../images/clash-royale-logo.png";
 
 function MainPage() {
   return (
@@ -50,6 +51,18 @@ function MainPage() {
           <img src={image} alt="Main Logo" />
         </Grid>
       </Grid>
+      <Box sx={{ padding: "10vw 0 0 0", textAlign: "center" }}>
+        <Typography
+          sx={{
+            fontFamily: "Roboto Mono",
+            color: "#777",
+            fontWeight: "300",
+          }}
+        >
+          Supported Games
+        </Typography>
+        <img src={crlogo} alt="Clash Royale" height="100" />
+      </Box>
       <Box
         sx={{
           margin: "10vw 0 0 0",
