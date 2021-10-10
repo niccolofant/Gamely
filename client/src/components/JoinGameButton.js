e9a93fa6d38073eb3a4af5f0ad245dc3787a73ec
ocintnaf <ocintnaf@gmail.com>
2021-10-10 18:05:13 +0200

    Create JoinGameButton.js

---
+++ client/src/components/JoinGameButton.js
@@ -0,0 +1,50 @@
+import { useState, useContext } from "react";
+import ButtonUnstyled from "@mui/core/ButtonUnstyled";
+import { styled } from "@mui/system";
+import Web3 from "web3";
+import authContext from "./authContext";
+
+const web3 = new Web3(Web3.givenProvider);
+
+const CustomButtonRoot = styled("button")(`
+  background-color: #5479F7;
+  padding: 15px 20px;
+  border-radius: 10px;
+  color: #fff;
+  font-weight: 400;
+  font-family: Roboto Mono, Arial, sans-serif;
+  font-size: 14px;
+  transition: all 200ms ease;
+  cursor: pointer;
+  box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 0 rgba(0, 127, 255, 0);
+  border: none;
+
+  &:hover {
+    background-color: black;
+  }
+`);
+
+function CustomButton(props) {
+  return <ButtonUnstyled {...props} component={CustomButtonRoot} />;
+}
+
+function JoinGameButton(props) {
+  const auth = useContext(authContext);
+
+  async function joinGame(t) {
+    t.preventDefault();
+
+    if (auth.authenticated) {
+      const accounts = await web3.eth.getAccounts();
+      const account = accounts[0];
+      console.log(accounts);
+      await props.game.methods.joinGame().send({
+        from: account,
+        value: props.bet,
+      });
+    }
+  }
+  return <CustomButton onClick={joinGame}>Join Game</CustomButton>;
+}
+
+export default JoinGameButton;
